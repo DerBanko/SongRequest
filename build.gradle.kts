5974f08789b80f56be4f279490ae5668aa56a5fb
DerBanko <accounts@banko.tv>
2024-05-16 21:04:56 +0200

    Update twitch4j lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ repositories {
 
 dependencies {
     implementation(platform("com.squareup.okhttp3:okhttp-bom:4.11.0"))
-    implementation("com.github.twitch4j:twitch4j:1.16.0")
+    implementation("com.github.twitch4j:twitch4j:1.20.0")
     implementation("com.google.code.gson:gson:2.8.9")
-    implementation("ch.qos.logback:logback-classic:1.3.5")
+    implementation("ch.qos.logback:logback-classic:1.4.12")
 }
 
 java {
